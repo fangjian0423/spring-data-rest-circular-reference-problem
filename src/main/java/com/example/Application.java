cd25cffaadc03c002be79d99d90bc68daefe6d59
Jimmy Fang <fangjian0423@gmail.com>
2022-10-24 19:51:04 +0800

    simplify struction

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,8 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/com/example/CustomWebMvcConfigurer.java
+++ src/main/java/com/example/CustomWebMvcConfigurer.java
@@ -1,15 +1,23 @@
 package com.example;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Component
 public class CustomWebMvcConfigurer implements WebMvcConfigurer {
 
-    private final HttpClient httpClient;
+    private final RestTemplateBuilder restTemplateBuilder;
 
-    public CustomWebMvcConfigurer(HttpClient httpClient) {
-        this.httpClient = httpClient;
+    public CustomWebMvcConfigurer(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        // adding OAuth2AuthorizedClientArgumentResolver, it depends on global RestTemplateBuilder
+    }
 }
